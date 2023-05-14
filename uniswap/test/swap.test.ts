c604e83e5888bc5be90049bfc4c33351797bca91
Lloyd Montero <catchphilippines@gmail.com>
2023-05-14 21:44:26 +0800

    updated test script

--- uniswap/test/swap.test.ts
+++ uniswap/test/swap.test.ts
@@ -1,19 +1,16 @@
 import { ethers } from "hardhat";
 import { Contract, ContractFactory, BigNumber } from "ethers";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-
-const DAI = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
-const WETH9 = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
-const USDC = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
+import { router, token1, token2, token3 } from "./common/constant";
 
 describe("SwapExamples", () => {
   let accounts: SignerWithAddress[] = [];
   let signer1: SignerWithAddress;
 
   let swapExamples: Contract;
-  let weth: Contract;
-  let dai: Contract;
-  let usdc: Contract;
+  let token2Contract: Contract;
+  let token1Contract: Contract;
+  let token3Contract: Contract;
 
   let balance: BigNumber;
 
@@ -24,77 +21,116 @@ describe("SwapExamples", () => {
     const SwapExamples: ContractFactory = await ethers.getContractFactory(
       "SwapExamples"
     );
-    swapExamples = await SwapExamples.deploy();
+    swapExamples = await SwapExamples.deploy(router, token1, token2, token3);
     await swapExamples.deployed();
 
-    weth = await ethers.getContractAt("IWETH", WETH9);
-    dai = await ethers.getContractAt("IERC20", DAI);
-    usdc = await ethers.getContractAt("IERC20", USDC);
+    // Check if the contract is stable coin
+    if (token2 == "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2") {
+      token2Contract = await ethers.getContractAt("IWETH", token2);
+    } else {
+      token2Contract = await ethers.getContractAt("IERC20", token2);
+    }
+
+    token1Contract = await ethers.getContractAt("IERC20", token1);
+    token3Contract = await ethers.getContractAt("IERC20", token3);
   });
 
   it("swapExactInputSingle", async () => {
-    const amountIn = 10n ** 18n;
+    const amountIn: bigint = 10n ** 18n; // 1
+    const amountOutMinimum: number = 0;
+    const fee1: number = 3000;
+    const sqrtPriceLimitX96: number = 0;
 
-    // Deposit WETH
-    await weth.deposit({ value: amountIn });
-    await weth.approve(swapExamples.address, amountIn);
+    // Deposit token2Contract
+    await token2Contract.deposit({ value: amountIn });
+    await token2Contract.approve(swapExamples.address, amountIn);
 
     // Swap
-    await swapExamples.swapExactInputSingle(amountIn);
+    await swapExamples.swapExactInputSingle(
+      amountIn,
+      amountOutMinimum,
+      fee1,
+      sqrtPriceLimitX96
+    );
 
     // format balance
-    balance = await dai.balanceOf(accounts[0].address);
+    balance = await token1Contract.balanceOf(accounts[0].address);
 
-    console.log("DAI balance", ethers.utils.formatEther(balance.toString()));
+    console.log("token1 balance", ethers.utils.formatEther(balance.toString()));
   });
 
   it("swapExactOutputSingle", async () => {
-    const wethAmountInMax = 10n ** 18n;
-    const daiAmountOut = 100n * 10n ** 18n;
+    const token2ContractAmountInMax: bigint = 10n ** 18n; // 1
+    const token1ContractAmountOut: bigint = 100n * 10n ** 18n; // 100
+    const fee1: number = 3000;
+    const sqrtPriceLimitX96: number = 0;
 
-    // Deposit WETH
-    await weth.deposit({ value: wethAmountInMax });
-    await weth.approve(swapExamples.address, wethAmountInMax);
+    // Deposit token2Contract
+    await token2Contract.deposit({ value: token2ContractAmountInMax });
+    await token2Contract.approve(
+      swapExamples.address,
+      token2ContractAmountInMax
+    );
 
     // Swap
-    await swapExamples.swapExactOutputSingle(daiAmountOut, wethAmountInMax);
+    await swapExamples.swapExactOutputSingle(
+      token1ContractAmountOut,
+      token2ContractAmountInMax,
+      fee1,
+      sqrtPriceLimitX96
+    );
 
     // format balance
-    balance = await dai.balanceOf(accounts[0].address);
+    balance = await token1Contract.balanceOf(accounts[0].address);
 
-    console.log("DAI balance", ethers.utils.formatEther(balance.toString()));
+    console.log("token1 balance", ethers.utils.formatEther(balance.toString()));
   });
 
   it("swapExactInputMultihop", async () => {
-    const amountIn = 10n ** 18n;
+    const amountIn = 10n ** 18n; // 1
+    const fee1: number = 3000;
+    const fee2: number = 100;
 
-    // Deposit WETH
-    await weth.deposit({ value: amountIn });
-    await weth.approve(swapExamples.address, amountIn);
+    // Deposit token2Contract
+    await token2Contract.deposit({ value: amountIn });
+    await token2Contract.approve(swapExamples.address, amountIn);
 
     // Swap
-    await swapExamples.swapExactInputMultihop(amountIn);
+    await swapExamples.swapExactInputMultihop(amountIn, fee1, fee2);
 
     // format balance
-    balance = await dai.balanceOf(accounts[0].address);
+    balance = await token1Contract.balanceOf(accounts[0].address);
 
-    console.log("DAI balance", ethers.utils.formatEther(balance.toString()));
+    console.log("token1 balance", ethers.utils.formatEther(balance.toString()));
   });
 
   it("swapExactOutputMultihop", async () => {
-    const wethAmountInMax = 10n ** 18n;
-    const daiAmountOut = 100n * 10n ** 18n;
+    const token2ContractAmountInMax = 10n ** 18n; // 1
+    const token1ContractAmountOut = 100n * 10n ** 18n; // 100
+    const fee1: number = 100;
+    const fee2: number = 3000;
 
-    // Deposit WETH
-    await weth.deposit({ value: wethAmountInMax });
-    await weth.approve(swapExamples.address, wethAmountInMax);
+    // Deposit token2Contract
+    await token2Contract.deposit({ value: token2ContractAmountInMax });
+    await token2Contract.approve(
+      swapExamples.address,
+      token2ContractAmountInMax
+    );
 
     // Swap
-    await swapExamples.swapExactOutputMultihop(daiAmountOut, wethAmountInMax);
+    await swapExamples.swapExactOutputMultihop(
+      token1ContractAmountOut,
+      token2ContractAmountInMax,
+      fee1,
+      fee2
+    );
 
     // format balance
-    balance = await dai.balanceOf(accounts[0].address);
+    balance = await token1Contract.balanceOf(accounts[0].address);
 
-    console.log("DAI balance", ethers.utils.formatEther(balance.toString()));
+    console.log(
+      "token1 balance",
+      ethers.utils.formatEther(balance.toString())
+    );
   });
 });
